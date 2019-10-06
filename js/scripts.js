1f0c874c5523f614d91d9a769860f1a99874cef7
dixonjerryjr <47995026+dixonjerryjr@users.noreply.github.com>
2019-10-06 15:23:24 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,35 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+$('.menu-toggle').click(function(){
+
+ $('.site-nav').toggleClass('site-nav--open', 500);
+ $(this).toggleClass('open');
+})
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
